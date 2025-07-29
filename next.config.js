464d2632b6c3dabab6bd7fe320da8f27f938bbd6
classicwinesflorida <classicwinesflorida@gmail.com>
2025-07-29 19:10:05 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const isProd = process.env.NODE_ENV === "production";
+
     return [
       {
         source: "/api/go/:path*",
-        destination: "http://localhost:3000/api/:path*",
+        destination: isProd
+          ? "https://your-backend-service-url/api/:path*" // Use actual BE URL in prod
+          : "http://localhost:3000/api/:path*",
       },
     ];
   },
