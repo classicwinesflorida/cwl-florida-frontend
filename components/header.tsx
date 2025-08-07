714d8c30b9468bbcc690b7a27c6bc4e823073fc1
vikaspandey-techsierra <vikas.pandey@techsierra.in>
2025-08-07 16:06:02 +0530

    updated base url

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8080";
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
 
 interface User {
   name: string;
